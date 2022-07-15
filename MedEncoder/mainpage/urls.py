900b6650567b88e25c0b192c9b5ad1d6db8ecdb9
Angelo <swailem.lumba@obf.ateneo.edu>
2022-07-15 23:04:38 +0800

    Polished up views + urls.py. Also added templates (content to be added later).

--- MedEncoder/mainpage/urls.py
+++ MedEncoder/mainpage/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import PatientView, detail, add_patient
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', PatientView.as_view(), name='index'),
+    path('<int:patient_id>/details', detail, name="detail"),
+    path('add/', add_patient, name="add_patient"),
 ]
 
 app_name = "mainpage"
