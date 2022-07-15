541fce641a62dd5515482837b25b1558937aadaa
Angelo <swailem.lumba@obf.ateneo.edu>
2022-07-15 22:46:47 +0800

    added templates + cleaned some apps.

--- MedEncoder/MedEncoder/settings.py
+++ MedEncoder/MedEncoder/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ty(v2^un4rv2ul@0%5pd^9$gk#dhtdx_ht#6_&9wj2(0d_w7i9'
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainpage',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,7 @@ ROOT_URLCONF = 'MedEncoder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'MedEncoder/static')]
+
+#Media Files
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'MedEncoder/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- MedEncoder/mainpage/models.py
+++ MedEncoder/mainpage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
 # Create your models here.
+
+class Patient(models.Model):
+    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    diagnosis = models.TextField()
+    last_checkup = models.DateField()
+    sex = models.CharField(max_length=1)
+    lab_results_date = models.DateField()
+    address = models.CharField(max_length=100)
+    lab_results_img = models.ImageField(upload_to='mainpage/images')
