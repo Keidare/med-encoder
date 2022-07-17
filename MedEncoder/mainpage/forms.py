5e736e4d3b18bf1291f66014fe5bd03d3ec657d5
Angelo <swailem.lumba@obf.ateneo.edu>
2022-07-18 00:36:34 +0800

    Fixed image not loading problem.

--- MedEncoder/mainpage/forms.py
+++ MedEncoder/mainpage/forms.py
@@ -6,3 +6,13 @@ class PatientForm(ModelForm):
     class Meta:
         model = Patient
         fields = ["last_name", "first_name", "address", "sex", "diagnosis", "last_checkup", "lab_results_date", "lab_results_img"]
+        widgets = {
+            'last_name': forms.TextInput(attrs={'class':'form-control'}),
+            'first_name': forms.TextInput(attrs={'class':'form-control'}),
+            'address': forms.TextInput(attrs={'class':'form-control'}),
+            'sex': forms.TextInput(attrs={'class':'form-control'}),
+            'diagnosis': forms.Textarea(attrs={'class':'form-control'}),
+            'last_checkup': forms.DateInput(attrs={'class':'form-control'}),
+            'lab_results_date': forms.DateInput(attrs={'class':'form-control'}),
+            'lab_results_img': forms.FileInput(attrs={'class':'form-control'})
+        }
